6eb0e73ae2b7646ba3a1c7941775b49ee21e1121
voiceless-zell <161730934+voiceless-zell@users.noreply.github.com>
2025-03-06 11:49:18 -0600

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,13 +3,14 @@
 
   inputs = {
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
     nvf.url = "github:notashelf/nvf";
     stylix.url = "github:danth/stylix/release-24.11";
   };
+
 
   outputs = {nixpkgs, ...} @ inputs: let
     system = "x86_64-linux";
