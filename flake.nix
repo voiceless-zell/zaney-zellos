5c37d18dad2f316c8c59e452e4ca2346b2fee811
voiceless-zell <peter.bouchard2893@proton.me>
2025-03-06 06:44:08 -0500

    update to unstable

--- flake.nix
+++ flake.nix
@@ -3,10 +3,10 @@
 
   inputs = {
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nvf.url = "github:notashelf/nvf";
     stylix.url = "github:danth/stylix/release-24.11";
   };
