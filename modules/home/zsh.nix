f5eb8866a0b24d22e448abbe9c7900e15ea08139
voiceless-zell <peter.bouchard2893@proton.me>
2025-03-05 19:23:49 -0500

    add alias

--- modules/home/zsh.nix
+++ modules/home/zsh.nix
@@ -18,6 +18,7 @@
       c = "clear";
       fr = "nh os switch --hostname ${profile}";
       fu = "nh os switch --hostname ${profile} --update";
+      fl = "cd ~/zaney-zellos/ && v";
       zu = "sh <(curl -L https://gitlab.com/Zaney/zaneyos/-/raw/main/install-zaneyos.sh)";
       ncg = "nix-collect-garbage --delete-old && sudo nix-collect-garbage -d && sudo /run/current-system/bin/switch-to-configuration boot";
       cat = "bat";
