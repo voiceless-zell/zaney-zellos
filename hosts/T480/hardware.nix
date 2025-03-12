eab345b5c4d4c0c1ff7b533e3153150f2b45244e
voiceless-zell <peter.bouchard2893@proton.me>
2025-03-12 10:16:14 -0400

    change hosts

--- hosts/T480/hardware.nix
+++ hosts/T480/hardware.nix
@@ -35,6 +35,8 @@
   networking.useDHCP = lib.mkDefault true;
   # networking.interfaces.docker0.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp0s31f6.useDHCP = lib.mkDefault true;
+  # networking.interfaces.virbr0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.vnet0.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlp3s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
