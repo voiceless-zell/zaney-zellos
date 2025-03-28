d188b4b68691bf29815ff514d0fefab678785aaa
voiceless-zell <peter.bouchard2893@proton.me>
2025-03-28 13:14:18 -0400

    move star citizen file for global imports

---
+++ modules/core/starCitizen.nix
@@ -0,0 +1,26 @@
+{
+  # Cachix setup
+  nix.settings = {
+    substituters = ["https://nix-citizen.cachix.org"];
+    trusted-public-keys = ["nix-citizen.cachix.org-1:lPMkWc2X8XD4/7YPEEwXKKBg+SVbYTVrAaLA2wQTKCo="];
+  };
+  nix-citizen.starCitizen = {
+    # Enables the star citizen module
+    enable = true;
+    # Additional commands before the game starts
+    preCommands = ''
+      export DXVK_HUD=compiler;
+      export MANGO_HUD=1;
+    '';
+    # Experimental script
+    helperScript.enable = true;
+
+    # # This option is enabled by default
+    # #  Configures your system to meet some of the requirements to run star-citizen
+    # # Set `vm.max_map_count` default to `16777216` (sysctl(8))
+    # #Set `fs.file-max` default to `524288` (sysctl(8))
+    # #Also sets `security.pam.loginLimits` to increase hard (limits.conf(5))
+    # # Changes outlined in  https://github.com/starcitizen-lug/knowledge-base/wiki/Manual-Installation#prerequisites
+    # setLimits = false;
+  };
+}
