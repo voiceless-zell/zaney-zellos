42c29039f7bd3e68e80aa649e045096b191f000d
voiceless-zell <161730934+voiceless-zell@users.noreply.github.com>
2025-04-27 13:57:17 -0400

    Update nvf.nix

--- modules/home/nvf.nix
+++ modules/home/nvf.nix
@@ -115,7 +115,6 @@
         trouble.enable = true;
         lspSignature.enable = true;
         otter-nvim.enable = false;
-        lsplines.enable = false;
         nvim-docs-view.enable = false;
       };
 
