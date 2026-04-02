aee467d98b371674dcadc2a9ac2c2a76881cabd8
Gremlin Prime <gremlin-prime@local.invalid>
2026-04-02 10:36:21 -0400

    Set VLC as default XDG media player

--- modules/home/xdg.nix
+++ modules/home/xdg.nix
@@ -7,5 +7,30 @@
       createDirectories = true;
       setSessionVariables = true;
     };
+
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "audio/aac" = [ "vlc.desktop" ];
+        "audio/flac" = [ "vlc.desktop" ];
+        "audio/mpeg" = [ "vlc.desktop" ];
+        "audio/mp4" = [ "vlc.desktop" ];
+        "audio/ogg" = [ "vlc.desktop" ];
+        "audio/wav" = [ "vlc.desktop" ];
+        "audio/webm" = [ "vlc.desktop" ];
+        "video/3gpp" = [ "vlc.desktop" ];
+        "video/mp2t" = [ "vlc.desktop" ];
+        "video/mp4" = [ "vlc.desktop" ];
+        "video/mpeg" = [ "vlc.desktop" ];
+        "video/ogg" = [ "vlc.desktop" ];
+        "video/quicktime" = [ "vlc.desktop" ];
+        "video/webm" = [ "vlc.desktop" ];
+        "video/x-matroska" = [ "vlc.desktop" ];
+        "video/x-msvideo" = [ "vlc.desktop" ];
+        "x-content/video-dvd" = [ "vlc.desktop" ];
+        "x-content/video-vcd" = [ "vlc.desktop" ];
+        "x-content/video-svcd" = [ "vlc.desktop" ];
+      };
+    };
   };
 }
